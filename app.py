33a022d0619103bba7bb797fb3cf0fae34baf5a0
Deepanshu Surania <90096711+SuraniaD@users.noreply.github.com>
2025-11-19 05:18:53 +0000

    changes

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
-from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchRun
-from langchain.agents import initialize_agent,AgentType
+from langchain_community.utilities import ArxivAPIWrapper, WikipediaAPIWrapper
+from langchain_community.tools import ArxivQueryRun, WikipediaQueryRun, DuckDuckGoSearchRun
+
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.callbacks import StreamlitCallbackHandler
+from langchain_core.tools import BaseTool  # if needed
 import os
 from dotenv import load_dotenv
 
